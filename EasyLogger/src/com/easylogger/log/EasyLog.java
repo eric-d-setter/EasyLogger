15cce67d029bb4f995c22af1f82768cc8b8a611b
eric-dybsetter <eric.dybsetter@gmail.com>
2024-01-03 14:33:59 -0500

    Adding preliminary work for log levels

--- EasyLogger/src/com/easylogger/log/EasyLog.java
+++ EasyLogger/src/com/easylogger/log/EasyLog.java
@@ -1,18 +1,20 @@
 package com.easylogger.log;
 
 import java.time.Instant;
-import java.util.Date;
 
 public class EasyLog {
 
     private static final String EMPTY = "";
+    public static final Output INFO = Output.INFO;
+    public static final Output WARN = Output.WARN;
+    public static final Output ERR = Output.ERR;
 
     /**
      * Logs the current method and the prior calling method.
      * @param logMessage User customizable message
      */
     public static void logBreadcrumbs(String logMessage) {
-        logBreadcrumbs(Output.OUT, 1, EMPTY, logMessage);
+        logBreadcrumbs(Output.INFO, 1, EMPTY, logMessage);
     }
 
     /**
@@ -21,7 +23,7 @@ public class EasyLog {
      * @param logMessage User customizable message
      */
     public static void logBreadcrumbs(int depth, String logMessage) {
-        logBreadcrumbs(Output.OUT, depth, EMPTY, logMessage);
+        logBreadcrumbs(Output.INFO, depth, EMPTY, logMessage);
     }
 
     /**
@@ -31,7 +33,7 @@ public class EasyLog {
      * @param logMessage User customizable message
      */
     public static void logBreadcrumbs(int depth, String prefix, String logMessage) {
-        logBreadcrumbs(Output.OUT, depth, prefix, logMessage);
+        logBreadcrumbs(Output.INFO, depth, prefix, logMessage);
     }
 
     /**
@@ -61,6 +63,13 @@ public class EasyLog {
         logBreadcrumbs(Output.ERR, depth, prefix, logMessage);
     }
 
+    /**
+     * Logs the current method and every calling method based on user input.
+     * @param output Type of log: Info, Warning, or Error
+     * @param depth The number of extra method names the user wishes to see
+     * @param prefix Tag for the log, useful for filtering
+     * @param logMessage User customizable message
+     */
     private static void logBreadcrumbs(Output output, int depth, String prefix, String logMessage) {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i <= depth; i++) {
@@ -74,7 +83,7 @@ public class EasyLog {
      * @param logMessage User customizable message
      */
     public static void logWithLocation(String logMessage) {
-        log(Output.OUT, "@" + MethodHelper.higherMethodName(), EMPTY, logMessage);
+        log(Output.INFO, "@" + MethodHelper.higherMethodName(), EMPTY, logMessage);
     }
 
     /**
@@ -83,7 +92,7 @@ public class EasyLog {
      * @param logMessage User customizable message
      */
     public static void logWithLocation(String prefix, String logMessage) {
-        log(Output.OUT, "@" + MethodHelper.higherMethodName(), prefix, logMessage);
+        log(Output.INFO, "@" + MethodHelper.higherMethodName(), prefix, logMessage);
     }
 
     /**
@@ -108,7 +117,7 @@ public class EasyLog {
      * @param logMessage User customizable message
      */
     public static void log(String logMessage) {
-        log(Output.OUT, EMPTY, EMPTY, logMessage);
+        log(Output.INFO, EMPTY, EMPTY, logMessage);
     }
 
     /**
@@ -117,7 +126,7 @@ public class EasyLog {
      * @param logMessage User customizable message
      */
     public static void log(String prefix, String logMessage) {
-        log(Output.OUT, EMPTY, prefix, logMessage);
+        log(Output.INFO, EMPTY, prefix, logMessage);
     }
 
     /**
--- EasyLogger/src/com/easylogger/log/Output.java
+++ EasyLogger/src/com/easylogger/log/Output.java
@@ -1,14 +1,18 @@
 package com.easylogger.log;
 
 import java.io.PrintStream;
+import java.util.logging.Level;
 
 public enum Output {
-    OUT(System.out),
-    ERR(System.err);
+    INFO(System.out, Level.INFO),
+    WARN(System.out, Level.WARNING),
+    ERR(System.err, Level.SEVERE);
 
     public final PrintStream output;
+    public final Level level;
 
-    private Output(PrintStream output) {
+    private Output(PrintStream output, Level level) {
         this.output = output;
+        this.level = level;
     }
 }
